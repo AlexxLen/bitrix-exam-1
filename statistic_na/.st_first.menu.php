4fd43d57819d0df2a23c254840e56c49a151a0d5
Alexander Lenchenkov <lenchenkov.a.d@gmail.com>
2026-02-23 20:47:40 +0300

    ex1-510: Базовые компоненты - Меню

--- statistic_na/.st_first.menu.php
+++ statistic_na/.st_first.menu.php
@@ -4,28 +4,28 @@ $aMenuLinks = Array(
 		"Дашборд", 
 		"/statistic_na/dashboard/", 
 		Array(), 
-		Array(), 
+		Array("menu_ico"=>"bi-grid"), 
 		"" 
 	),
 	Array(
 		"Основные", 
 		"/statistic_na/main/", 
 		Array(), 
-		Array(), 
+		Array("menu_ico"=>"bi-menu-button-wide"), 
 		"" 
 	),
 	Array(
 		"Дополнительные", 
 		"/statistic_na/extra/", 
 		Array(), 
-		Array(), 
+		Array("menu_ico"=>"bi-files"), 
 		"" 
 	),
 	Array(
 		"Профиль", 
 		"/statistic_na/profile/", 
 		Array(), 
-		Array(), 
+		Array("menu_ico"=>"bi-person"), 
 		"" 
 	)
 );
