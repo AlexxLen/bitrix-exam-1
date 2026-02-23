2b36d80680a272045374e929bdeeb94862aae0e5
Alexander Lenchenkov <lenchenkov.a.d@gmail.com>
2026-02-23 23:16:31 +0300

    ex1-810: Раздел сайта с динамической информацией и ЧПУ

---
+++ local/templates/exam1_type2/components/bitrix/news/table/bitrix/news.detail/table/lang/ru/template.php
@@ -0,0 +1,7 @@
+<?
+$MESS['DETAIL_ID'] = 'ID';
+$MESS['DETAIL_PRODUCT'] = 'Товар';
+$MESS['PRODUCT_CATEGORY'] = 'Категория';
+$MESS['DETAIL_CITY'] = 'Город';
+$MESS['DETAIL_QUANTITY'] = 'Количество';
+$MESS['BACK_TO_LIST'] = 'Назад к списку';
---
+++ local/templates/exam1_type2/components/bitrix/news/table/bitrix/news.list/table/lang/ru/template.php
@@ -0,0 +1,7 @@
+<?
+$MESS["ITEM_NUMBER"] = "№";
+$MESS["ITEM_PRODUCT"] = "Товар";
+$MESS["ITEM_CATEGORY"] = "Категория";
+$MESS["ITEM_CITY"] = "Город";
+$MESS["ITEM_QUANTITY"] = "Количество";
+?>
